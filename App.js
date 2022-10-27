dc27a108100d9bea2d702d83d0b577908a1b464f
weealDCTEF <elynetobr@outlook.com>
2022-10-27 09:58:40 -0300

    new version 0.0.1

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [valor1,setValor1] = useState(0);
+  const [valor2,setValor2] = useState(0);
+  let resultado = 0;
+
+
+  const somar = ()=>{
+      resultado = parseFloat(valor1) - parseFloat(valor2);
+      alert('O resultado é: ' + resultado);
+          
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <TextInput
+      placeholder = 'Digite o primeiro valor'
+      value = {valor1}
+      onChangeText = {valor1 =>setValor1(valor1)}
+      keyboardType = 'numeric'
+    >
+    </TextInput>
+
+    <TextInput
+      placeholder = 'Digite o segundo valor'
+      value = {valor2}
+      onChangeText = {valor2 =>setValor2(valor2)}
+      keyboardType = 'numeric'
+    >
+    </TextInput>
+
+    <Button
+      title = 'Somar'
+      onPress = {somar}
+      color = '#841584'
+    >
+    </Button>
+    <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
 });
